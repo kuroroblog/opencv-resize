32c2027e829487ca3de4a7782e8d89ef7da8e729
tocchimittsu <arimagedon21@gmail.com>
2021-09-02 11:28:21 +0900

    修正

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 # imread : 画像ファイルを読み込んで、多次元配列(numpy.ndarray)にする。
 # imreadについて : https://kuroro.blog/python/wqh9VIEmRXS4ZAA7C4wd/
 # 第一引数 : 画像のファイルパス
-# 戻り値 : 行(高さ) x 列(幅) x 色の三次元配列(numpy.ndarray)が返される。
+# 戻り値 : 行 x 列 x 色の三次元配列(numpy.ndarray)が返される。
 img = cv2.imread('./input.jpg')
 
 # 画像ファイルが正常に読み込めなかった場合、プログラムを終了する。
@@ -65,4 +65,3 @@ img = cv2.resize(img, (int(width * 0.5), int(height * 0.5)))
 # ]
 # imwriteについて : https://kuroro.blog/python/i0tNE1Mp8aEz8Z7n6Ggg/
 cv2.imwrite('output.jpg', img)
-
